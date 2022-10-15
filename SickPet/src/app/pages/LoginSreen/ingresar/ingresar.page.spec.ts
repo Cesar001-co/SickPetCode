a46c807c34f0575effbd86046f12526788222f4e
Cesar001-co <cesarcamilo001@gmail.com>
2022-10-15 18:14:40 -0500

    updated

--- SickPet/src/app/pages/LoginSreen/ingresar/ingresar.page.spec.ts
+++ SickPet/src/app/pages/LoginSreen/ingresar/ingresar.page.spec.ts
@@ -1,24 +1,36 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { IngresarPage } from './ingresar.page';
 
 describe('IngresarPage', () => {
   let component: IngresarPage;
   let fixture: ComponentFixture<IngresarPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ IngresarPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(IngresarPage);
+    router = TestBed.get(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to new-password', () => {
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    expect(router.navigate).toHaveBeenCalledWith(['/new-password']);
   });
 });
