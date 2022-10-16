aba3f8b86dffc106ec5b947455b13d0540a7174b
Cesar001-co <cesarcamilo001@gmail.com>
2022-10-15 20:21:10 -0500

    updated

---
+++ SickPet/src/app/pages/LoginSreen/ingresar/ingresar.page.form.spec.ts
@@ -0,0 +1,15 @@
+// import { FormBuilder } from "@angular/forms";
+// import { IngresarPageForm } from "./ingresar.page.form";
+
+
+// describe('IngresarPageForm', () => {
+
+//     it('should create login form empty', () => {
+//         const ingresarPageForm = new IngresarPageForm(new FormBuilder());
+//         const form = ingresarPageForm.createForm();
+
+//         expect(form).not.toBeNull();
+//         expect(form.get('email')).not.toBeNull();
+//     });
+
+// })
---
+++ SickPet/src/app/pages/LoginSreen/ingresar/ingresar.page.form.ts
@@ -0,0 +1,16 @@
+// import { FormBuilder, FormGroup } from "@angular/forms";
+
+
+// export class IngresarPageForm {
+//     private formBuilder: FormBuilder;
+
+//     constructor(formBuilder: FormBuilder){
+//         this.formBuilder = formBuilder;
+//     }
+
+//     createForm(): FormGroup {
+//         return this.formBuilder.group({
+//             email: ['']
+//         });
+//     }
+// }sda
